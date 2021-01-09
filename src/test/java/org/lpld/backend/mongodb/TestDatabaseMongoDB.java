2c6f227b45751f5fcce1fdf6ee7c433bc4e8de6a
David-Charlesia <pro.charlesia.david@outlook.com>
2021-01-09 22:47:05 +0100

    r

--- src/test/java/org/lpld/backend/mongodb/TestDatabaseMongoDB.java
+++ src/test/java/org/lpld/backend/mongodb/TestDatabaseMongoDB.java
@@ -25,7 +25,7 @@ public class TestDatabaseMongoDB {
         MongoDatabase database = db.getDataBase();
         MongoCollection<Document> collection = database.getCollection("table");
         //int count = collection.count();
-        FindIterable<Document> myDoc = database.getCollection("table").find();
+        FindIterable<Document> myDoc = database.getCollection("table").find(new Document("nom","adam"));
         /*Document doc = new Document("name", "MongoDB").append("type", "database").append("count", 1)
                 .append("versions", Arrays.asList("v3.2", "v3.0", "v2.6"))
                 .append("info", new Document("x", 203).append("y", 102));
